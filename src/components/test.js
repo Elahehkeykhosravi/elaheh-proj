9e51988003fdc7710e39d8deaaa9e9a7f566e51a
Elaheh Keykhosravi <keykhosravielaheh@gmail.com>
2024-05-20 00:06:06 +0330

    test.api

---
+++ src/components/test.js
@@ -0,0 +1,138 @@
+import { BrowserRouter } from "react-router-dom";
+
+function testi (){
+
+
+
+    const myURL = 'api.example.com/test'
+    const myObject = false;
+
+    fetch ( ( ( ( 'URL' ) ) )) ;
+
+    // ma harchi paraantez bezarim mohen nist. doroste?
+    //yes
+    // hala chizi k to gofti in bood
+
+    fetch ( (myURL+22)+'testURL' , fetch("URL", 22) );
+    // ino gofti dg?
+    //are
+
+    // syntax mige: 
+    // function esmeFunction ( argumentha  )
+    //mazoret ine ke function((''),(""))
+    //are?// aafarin
+    // har function faghat 1 parantez jolosh dare. 
+    // asan function yani ye kalameyi k ye parantez joloshe. 
+    // har ja ye kalame didi k jolosh ye parantez baazo baste shode bood, yani k un ye functione.
+    //ok got it.
+    // hala, goftim parantez vaase khoshgelie, doroste?
+    // parantez bakhshi az syntax nist. sysntax mige: function esmeFunction ()
+    // dige daakhe un parantez, niaz b parantez dore har argument nadari.
+    //ohh eyval yani ba giyome jodashon mikonim?
+    // na, ba vigool
+    //na na ono fahmidam manzoram be hjaye paranteze dakhele?
+    // in alan doroste...
+    //soali bood?
+    // khabari bood
+    //eyval are doroste
+    // pas hala soaleto k gofti, ba mesal begu.
+    //yani mishe function("url","data")
+    // alan in chizi k to neveshti, yani ye functioni dari k 2 ta argument behesh dadi
+    // argument aval va dovom, jofteshun string hastan, chon k giyume daran. 
+    // giyume faghat vase ine k neshun bede stringe
+    //fahmidam
+    //aln inam 2ta argument dare?/
+    // dakhele parantez, ba virgul, chanta chiz az ham joda shodan?
+    //are
+    // chanta?
+    //2ta
+    // eyval
+    // alan chi?
+    //2ta?
+     // yes
+     // chon hanuz ba virgul 2 ta chiz az ham joda shodan, doroste?
+     //are
+     // alan chi?
+     //2tq?
+
+     // yes, hanuzam ye function dari k tush ye virgul dari k 2 ta argumento az ham joda karde
+     // hala in argument ha mitune pichide bashe.... mitune kheeeeyli pichide bashe!
+     // tu daftaret ye mesal az argumente dovom ovordi, fkr kon in sheklie:
+
+     fetch ("URL" , [ { URL: "tesi"},{ method: "GET" },{auth: "TOKEN"}] )
+
+     // masalan ye hamchin chizio nega kon
+     // dar nahayat mishe in sheki b ghazie nega kard
+     fetch (  ("URL") , ([ { URL: "tesi"},{ method: "GET" },{auth: "TOKEN"}]) )
+    // yani: 
+    fetch ( () , () );
+
+    // k mishe hamun 2 ta argument..
+    //ohh fahmidam
+
+    // hala man khodam doos nadaram functionaam kasif bashan. 
+    // man haminshe miam injoori minivisam:
+
+    const myAPIUrl = 'testapi.url.com';
+    const myTestObject = {
+        type: "GET",
+        token: "MYTOKENHASH"
+        // va hala har chize digeyi k in objecte niaz dare
+
+    };
+
+
+    fetch (myAPIUrl , myTestObject);
+    // mibini mouse ro rush negah midari miare k chie?
+    //yani to vseye api const tarif kardi?
+    //badam toye fetch faghat calleshon kardi?
+    // are, va miam un myAPI ro balaye safe tarif mikonam, 
+    // shayad functione fetch, khate 1631om call shode bashe, ki haal dare badan peydaash kone! vali 
+    // man api ro balaye safe tarif mikoanm, k dar dast res bashe. 
+    // ham code khaanaa tar mishe, ham editesh raahat tar mishe, ham badan yeki mikhast bebinatesh rahat bashe, va fosham nade :)
+    //sahih
+    //hi
+    // hello
+    // fetch bedune argumente dovom, GET anjaam mide
+    // amalan vaseye GET, faghat ye URL niaz dari.
+    //are.
+    // pas alan to hadeaghal get mituni anjaam bedi...
+    // POST mimune (baghiashunam badanam mishe yaad gereft, kam estefade mishan.)
+    //// are to ye video youtube didam baghiyashi\ dasht migoft
+    //postam video ro dashtam mididam
+    // az man mishnavi, aval GET ro anjaam bede, data ro begir, promise ro anjaam bede 
+    // az then o catch estefade kon , va bbin k data miad o ba data yekam baazi kon. 
+    // badesh boro soraaghe POST.
+    //bashe
+    //documentamo didi?
+    // are didam, be github ham push kardam.
+    // to hamchenan goosh nemikoni behem, valiiiiiii, github moheme, nem
+    //midoonm faghaaaaatt yadam  mireeee
+    // bbin to alan mituni push koni, badan beri paak koni k chio push kardi, asan hamin document mitune push she o ye taaghiri tu profilet bede!
+    // kasi nemifahme k chi boode! faghat mibine k adame activi boodi.... 
+    //ok havasamo jam mikonm
+    // hamin alan in taghiraato doboare push kon bbinam
+    //taghiri nadadim ke
+    // alan man CR
+    //ok
+    //chie?
+    // baba koja miri 
+    // aval in safaro bayad save kon bb nemikham save konm
+    //hannoz safehaye dgam ok nist
+    //onaram save nakardam
+    // man b un safe ha kaari nadaram
+    // hamin file text dare taghir mikone dg, ino save kon o 
+    //chejori faghat ino save konm
+    //ono bezanm hame taghirat save mishe
+    // bezan natars
+    //bb emikham ee
+    //mn daram ghadam be ghadam pish miram
+    // zadam o ave kardm
+
+
+
+
+
+
+
+}
